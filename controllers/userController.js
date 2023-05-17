b9758f0b421f1d3c520edc5828520ded5f7ee73c
cunhafelipe <felipe.cunha2001@hotmail.com>
2023-05-17 20:09:49 -0300

    feat: creating user controller

---
+++ controllers/userController.js
@@ -0,0 +1,44 @@
+const User = require("../models/userModel.js");
+
+const UserController = {
+  async findUsers(request, response) {
+    try {
+      const user = await User.findOne();
+      response.json(user);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  async createUser(request, response) {
+    try {
+      const user = {
+        name: request.body.name,
+        email: request.body.email,
+        password: request.body.password,
+      };
+
+      const isEmailExists = await User.findOne({ email: user.email });
+
+      if (isEmailExists) {
+        response.satus(400).send("E-mail já criado");
+      }
+
+      if (user.password.length < 7) {
+        response.status(400).send("Senha pequena");
+      }
+
+      if (!user) {
+        response.status(400).send("Dados vazios");
+      }
+
+      return await User.create(user).then((userCreate) => {
+        response.status(200).json(userCreate);
+      });
+    } catch (error) {
+      response.status(200).send(error.message);
+    }
+  },
+};
+
+module.exports = UserController;
