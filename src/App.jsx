9dd5db56620283690da3601959ab202d6cdce6cd
cunhafelipe <felipe.cunha2001@hotmail.com>
2023-05-18 19:17:54 -0300

    feat: consuming API

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react";
+
 import TelaLogin from "./TelaLogin.jsx";
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/exemplo")
+      .then((response) => response.json())
+      .then((data) => {
+        setMessage(data.message);
+      });
+  }, []);
   return (
     <>
       <TelaLogin />
+      <p>{message}</p>
     </>
   );
 }
