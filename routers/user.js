671530e61158c932cc9dbbf31fa654f0f60a526e
cunhafelipe <felipe.cunha2001@hotmail.com>
2023-05-18 19:14:28 -0300

    feat: creating route for API test

--- routers/user.js
+++ routers/user.js
@@ -5,4 +5,10 @@ router.route("/users").get(UserController.findUsers);
 
 router.route("/user").post(UserController.createUser);
 
+router.get("/api/exemplo", (_request, response) => {
+  response.json({
+    message: "Conexão entre o back-end e o front-end estabelecida com sucesso!",
+  });
+});
+
 module.exports = router;
